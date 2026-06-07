519a2100264b33f0a608a04ead16c4d019498afd
Martin Georgiev <martin04ge@gmail.com>
2026-06-07 14:41:16 +0300

    Fix wakeup component bug.

--- assets/scripts/enemies/components/enemy_wakeup_component.gd
+++ assets/scripts/enemies/components/enemy_wakeup_component.gd
@@ -4,9 +4,7 @@ extends Component
 @onready var enemy: Enemy = get_owner()
 @export var wakeup_radius := 1000.0
 @export var sleep_radius := 1000.0
-@onready var is_awake: bool:
-	get():
-		return is_player_in_radius(wakeup_radius)
+@onready var is_awake: bool = is_player_in_radius(wakeup_radius)
 
 func _physics_process(_delta: float) -> void:
 	if not is_awake and is_player_in_radius(wakeup_radius):
@@ -14,6 +12,6 @@ func _physics_process(_delta: float) -> void:
 	elif is_awake and not is_player_in_radius(sleep_radius):
 		is_awake = false
 
-func is_player_in_radius(r: float):
+func is_player_in_radius(r: float) -> bool:
 	var player_offset = GameManager.player.global_position - enemy.global_position
 	return player_offset.length() <= r
